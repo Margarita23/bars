253371ec05c3c34f352a89b229f68bb419619b33
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2019-12-13 18:52:06 +0200

    add adaptive for bars

--- scripts/script.js
+++ scripts/script.js
@@ -2,45 +2,29 @@ const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 const barsList = document.querySelector('.bars__list');
 
-let menu = document.querySelector('.menu__list');
-let menuIcon = document.querySelector('.menu-icon__container');
-let menuCancel = document.querySelector('.menu__cancel');
-let menuCancelBefore = document.querySelector('.menu__cancel', ':before');
+let menuList = document.querySelector('.menu__list');
+let menuContainer = document.querySelector('.menu-icon__container');
+let menuOpen = document.querySelector('.menu-icon__open');
+let menuCancelContainer = document.querySelector('.menu-icon__cancel-container');
 
-let flagMenuExist = false;
+let flagMenuExist = true;
 
-menuIcon.addEventListener("click", function(event){
-    flagMenuExist = true;
-    // console.log(flagMenuExist);
-    showMenu();
+menuContainer.addEventListener("click", function(event){
+    showMenu(event);
 });
 
-function showMenu() {
+function showMenu(event) {
     if (flagMenuExist) {
-        // menu.style.display = "flex";
-        // menu.style.position = "fixed";
-        // menu.style.width = "100%";
-        // menu.style.height = "100vh";
-        // menu.style.top = "0";
-        // menu.style.left = "0";
-        // menu.style.flexDirection = "column";
-        // menu.style.justifyContent = "center";
-        // menu.style.alignItems = "center";
-        // menu.style.fontSize = "36px";
-        // menu.style.lineHeight = "72px";
-        // menu.style.fontWeight = "400";
-        // menu.style.backgroundColor = "#2f3234";
+        flagMenuExist = false;
+        menuOpen.style.display = "none";
+        menuCancelContainer.style.display = "block";
 
-        // menuCancel.style.display = "block";
-        // menuCancel.style.width = "30px";
-        // menuCancel.style.height = "30px";
-        // menuCancel.style.zIndex = "3";
-        // menuCancelBefore.style.display = "block";
-        // menuCancelBefore.style.position = "absolute";
-        // menuCancelBefore.style.backgroundColor = "red";
+        menuList.classList.add("menu__list--dark");
 
-        console.log(menuCancel);
-        console.log(menuCancelBefore);
+    } else {
+        flagMenuExist = true;
+        menuOpen.style.display = "block";
+        menuCancelContainer.style.display = "none";
     }
 }
 
