170b222eccc7d588e5657eef4efece67554f0a0d
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2019-12-25 13:13:31 +0200

    error with yandex maps

---
+++ src/scripts/script.js
@@ -0,0 +1,192 @@
+
+(function(){
+
+    ymaps.ready(init);
+
+    function init() {
+        let map = new ymaps.Map('map', {
+            center: [59.94, 30.32],
+            zoom: 12
+        });
+    }
+
+    const arrowLeft = document.querySelector('.arrow-left');
+    const arrowRight = document.querySelector('.arrow-right');
+    const barsList = document.querySelector('.bars__list');
+
+    let menuList = document.querySelector('.menu__list');
+    let menuContainer = document.querySelector('.menu-icon__container');
+    let menuOpen = document.querySelector('.menu-icon__open');
+    let menuCancelContainer = document.querySelector('.menu-icon__cancel-container');
+
+    let flagMenuExist = true;
+
+    menuContainer.addEventListener("click", function(event){
+        event.preventDefault();
+        showMenu(event);
+    });
+
+    function showMenu(event) {
+        if (flagMenuExist) {
+            flagMenuExist = false;
+            menuOpen.style.display = "none";
+            menuCancelContainer.style.display = "block";
+            menuList.classList.add("menu__list--dark");
+            document.body.style.overflow = "hidden";
+        } else {
+            flagMenuExist = true;
+            menuOpen.style.display = "block";
+            menuCancelContainer.style.display = "none";
+            menuList.classList.remove("menu__list--dark");
+            document.body.style.overflow = "";
+        }
+    }
+
+    arrowLeft.addEventListener("click", function(event){
+        loop("left");
+    });
+
+    arrowRight.addEventListener("click", function(event){
+        loop("right");
+    });
+
+    function loop(direction) {
+        if(direction === "right"){
+            barsList.appendChild(barsList.firstElementChild);
+        } else {
+            barsList.insertBefore(barsList.lastElementChild, barsList.firstElementChild);
+        }
+    }
+
+    //slide toggle
+    let team = document.querySelector('.team__list');
+
+    team.addEventListener('click', function(event) {
+        if(event.target.classList[0] == 'name') {
+            if(event.target.parentElement.classList[1]) {
+                event.target.parentElement.classList.remove('team__item--active');
+                return;
+            }
+            team.querySelectorAll('.team__item--active').forEach(
+                el => {
+                    el.classList.remove('team__item--active');
+                }
+            );
+            event.target.parentElement.classList.add('team__item--active');
+        }
+    });
+
+    //accordion toggle
+    let recipes = document.querySelector(".recipes__list");
+    let recipesItem = document.querySelector(".recipes__item--active");
+
+    recipes.addEventListener('click', function(event) {
+
+        if(event.target.className == 'recipes__cancel-btn') {
+            let item = event.target.parentElement.parentElement;
+            item.classList.remove('recipes__item--active');
+            removeRecipeDesc(event.target.parentElement);
+
+            if (window.innerWidth <= 768 && window.innerWidth >= 480) {
+                recipes.style.width = '70%';
+                recipes.parentElement.parentElement.querySelector('.subject--recipes').classList.remove('none-active');
+            }
+            // functional phone
+            //-------------------------------
+            if (window.innerWidth <= 480) {
+                recipes.querySelectorAll('.recipes__item').forEach(function(el){
+                    el.querySelector('.recipes__title').style.display = 'block';
+                });
+            return;
+            }
+        }
+
+        if(event.target.className == 'recipes__title') {
+            recipes.querySelectorAll('.recipes__item--active').forEach(function(item){
+                item.classList.remove('recipes__item--active');
+                removeRecipeDesc(item.querySelector('.recipes__desc'));
+                item.querySelector('.recipes__desc').classList.add('close-desc');
+            });
+            event.target.parentElement.classList.add('recipes__item--active');
+
+            // functional tablets
+            // -------------------------------
+            if (window.innerWidth <= 768) {
+                recipes.style.width = '100%';
+                recipes.parentElement.parentElement.querySelector('.subject--recipes').classList.add('none-active');
+            }
+
+            // functional phone
+            // -------------------------------
+            if (window.innerWidth <= 480) {
+                recipes.querySelectorAll('.recipes__title').forEach(el => {
+                    el.style.display = 'none';
+                });
+                event.target.parentElement.querySelector('.recipes__title').style.display = 'block';
+            }
+
+            let recipesDesc = event.target.nextElementSibling;
+            recipesDesc.style.width = "100%";
+            recipesDesc.style.overflow = 'visible';
+            recipesDesc.style.display = 'block';
+        }
+    });
+    
+    function removeRecipeDesc(desc){
+        desc.style.width = "0px";
+        desc.style.overflow = 'hidden';
+        desc.style.display = 'none';
+    }
+
+    //---------------------
+    //review switcher
+    let reviewsList = document.querySelector('.reviews__list');
+    let reviewShowList = document.querySelector('.review-show__list');
+
+    reviewsList.addEventListener('click', function(event){
+        if(event.target.className === 'previews' && event.target.className != 'previews--active'){
+            let indexOfNewActiveItem = getIdFromArray(reviewsList.querySelectorAll(".reviews__item"), event.target.parentElement);
+            let indexOfOldActiveItem = getIdFromArray(reviewsList.querySelectorAll(".previews"), reviewsList.querySelector(".previews.previews--active"));
+            reviewsList.querySelectorAll(".previews")[indexOfOldActiveItem].classList.remove("previews--active");
+            reviewsList.querySelectorAll(".previews")[indexOfNewActiveItem].classList.add("previews--active");
+            reviewShowList.children[indexOfOldActiveItem].classList.remove('review-show__item--active');
+            reviewShowList.children[indexOfNewActiveItem].classList.add('review-show__item--active');
+        }
+    });
+
+    function getIdFromArray(array, element){
+        let res = null;
+        array.forEach(function(value, index){
+            if(value == element){
+                res = index;
+            }
+        });
+        return res;
+    }
+
+    // --------------------
+    // form processing
+    const orderForm = document.getElementById("formOrder");
+    const orderBtn = document.querySelector('.form__button');
+
+    orderBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        let formData = new FormData();
+        formData.append('name', orderForm.elements.name.value);
+        formData.append('phone', orderForm.elements.phone.value);
+        formData.append('comment', orderForm.elements.comment.value);
+        formData.append('email', 'margarita.andriukhina@gmail.com');
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        
+        xhr.addEventListener('load', function(){
+            if(this.status == 200){
+                console.log("все ок!");
+            }
+        });
+        xhr.send(formData);
+    });
+
+
+})()
---
+++ src/scripts/scriptPartTwo.js
@@ -0,0 +1,75 @@
+// $(function(){
+
+//     var generateDotsForSideBar = function() {
+//         var allSections = $('.wrapper').find('.section');
+//         var sideBar = $('.side-bar');
+//         allSections.each(function() {
+//             var dot = $('<li>', {
+//                 attr : {
+//                     class : 'side-bar__item'
+//                 },
+//                 html : '<a href="#" class="side-bar__link"></a>'
+//             });
+//             sideBar.append(dot);
+//         }); 
+        
+        
+//         sideBar.find('li').first().addClass('side-bar__item--active');
+//     }
+
+//     generateDotsForSideBar();
+
+//     var moveSlide = function(container, slideNum) {
+//         var items = container.find('.section'),
+//             activeSlide = items.filter('.active'),
+//             reqItem = items.eq(slideNum),
+//             reqIndex = reqItem.index(),
+//             list = container.find('.scroll__list'),
+//             duration = 800;
+
+//         if(reqItem.length){ 
+//             list.clearQueue().animate({
+//                 'top' : -reqIndex * 100 + '%',
+//             }, duration, function(){
+//                 activeSlide.removeClass('active');
+//                 reqItem.addClass('active');
+
+//                 $('.side-bar').children().eq(activeSlide.index()).removeClass('side-bar__item--active');
+//                 $('.side-bar').children().eq(reqIndex).addClass('side-bar__item--active');
+//             });
+//         }
+//     }
+
+//     $('.wrapper').on('wheel', function(e){
+//         var $this = false;
+//         let direction = 'stay';
+
+//         if (e.originalEvent.deltaY >=100) { //down
+//             $this = true;
+//             direction = 'down';
+//         }
+            
+//         if(e.originalEvent.deltaY <= -100) { //up
+//             $this = true;
+//             direction = 'up';
+//         }
+
+//         if($this){
+//             var container = $('.wrapper'),
+//                 items = $('.section', container),
+//                 activeSection = items.filter('.active'),
+//                 existedItem, edgeItem, reqItem;
+
+//             if(direction === 'down') {
+//                 existedItem = activeSection.next();
+//                 edgeItem = items.first();
+//             }
+//             if(direction === 'up') {
+//                 existedItem = activeSection.prev();
+//                 edgeItem = items.last();
+//             }
+//             reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
+//             moveSlide(container, reqItem); 
+//         }
+//     });  
+// });
